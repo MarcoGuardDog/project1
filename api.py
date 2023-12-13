375c68fa158ba94d14fab5bece4cc703087bd629
diegoviruez <jose.diego.viruez.vargas@gmail.com>
2023-12-12 22:19:23 -0400

    arregle los cambios solicitados

--- api.py
+++ api.py
@@ -4,29 +4,18 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.route("/tele",methods=["post"])
-def send_tele():
+@app.route("/send_service_status",methods=["post"])
+def send_service_status ():
     data = request.get_json()
-    
     device_id = data.get("device_id",None)
     ip_publica = data.get("ip_publica",None)
     ip_privada =data.get("ip_privada", None)
     interfaces= data.get("interfaces",None)
 
-    
-
-
     print(device_id,ip_publica,ip_privada,interfaces)
 
-    return jsonify({"mensaje":"tele inviada"})
-
-
-
-  
+    return jsonify({"mensaje":"service se registro."})
+ 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
+    
