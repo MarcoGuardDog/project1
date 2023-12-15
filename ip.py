37294aa42c32e93fa3aa3a896997b79f830e8746
diegoviruez <jose.diego.viruez.vargas@gmail.com>
2023-12-14 20:33:12 -0400

    ya lo modifique

--- ip.py
+++ ip.py
@@ -7,8 +7,6 @@ def ip_public():
 def ip_privad():
     nombre_de_host = socket.gethostname()
     ip_privada = socket.gethostbyname(nombre_de_host)
-    print(ip_privada) 
+    return ip_privada 
 
-print("esta una ip_publica",ip_public())
 
-ip_privad()
